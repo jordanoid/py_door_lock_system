9ac78828b889f122afa30a2aa2b38fdd4edb197b
jordanoid <79011617+jordanoid@users.noreply.github.com>
2023-08-30 14:48:24 +0700

    qr code data publisher working

--- py_door_lock_system/qr_decoder_node.py
+++ py_door_lock_system/qr_decoder_node.py
@@ -4,18 +4,28 @@ import rclpy
 from rclpy.node import Node
 import cv2
 from sensor_msgs.msg import Image
+from std_msgs.msg import String
 from cv_bridge import CvBridge, CvBridgeError
+from pyzbar.pyzbar import decode
 
 class QRDecoder(Node):
     def __init__(self):
         super().__init__('QR_decoder_node')
-        self.subscription = self.create_subscription(Image, '/image_raw', self.decode_callback, 10)
-        self.subscription
+        self.image_sub = self.create_subscription(Image, '/image_raw', self.decode_callback, 10)
+        self.image_sub
+        self.qrdata_pub = self.create_publisher(String, 'camera/qrdata', 10)
         self.bridge = CvBridge()
         
     def decode_callback(self, msg):
-        self.get_logger().info("Receiving frame")
+        # self.get_logger().info("Receiving frame")
         frame = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
+        
+        detectedBarcodes = decode(frame)
+        if len(detectedBarcodes) > 0:
+            data_read = detectedBarcodes[0].data.decode()
+            qr_msg = String()
+            qr_msg.data = data_read
+            self.qrdata_pub.publish(qr_msg)
         
         cv2.imshow("camera", frame)
         cv2.waitKey(1)
