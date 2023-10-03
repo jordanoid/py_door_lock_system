e0b79c6e9d15d2adc47f8051f551cae0e1232eec
jordanoid-WSLubuntu <79011617+jordanoid@users.noreply.github.com>
2023-10-03 14:25:34 +0700

    scanner msg fix

--- py_door_lock_system/scanner_data_node.py
+++ py_door_lock_system/scanner_data_node.py
@@ -16,14 +16,17 @@ class Scanner(Node):
             depth=1
         )
         self.qrdata_pub = self.create_publisher(String, 'qrdata', qos_profile=qos_profile)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.data_publish)
         
 
     def data_publish(self):
+        decoded_data = String()
         try:
             ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)  # Adjust the port and baud rate as needed
             data = ser.readline()
             if data:
-                decoded_data = data.decode('utf-8')
+                decoded_data.data = data.decode('utf-8')
                 self.qrdata_pub.publish(decoded_data)
         except serial.SerialException as e:
             rclpy.get_logger().error(f"SerialException: {e}")
